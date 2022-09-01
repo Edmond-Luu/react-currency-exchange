b6ecaa60810e7eee2e72a5ddb39cac14b8ec1b04
Edmond Luu <eluu472@gmail.com>
2022-09-01 10:29:07 -0700

    Added icon

--- src/App.js
+++ src/App.js
@@ -3,91 +3,93 @@ import Form from "./Form"
 
 function App() {
 
-  const [fromVal, setFromVal] = React.useState()
-  const [fromSel, setFromSel] = React.useState("usd")
+  const [val, setVal] = React.useState(1)
+  const [inverse, setInverse] = React.useState(false)
+  const [rate, setRate] = React.useState()
 
-  const [toVal, setToVal] = React.useState()
-  const [toSel, setToSel] = React.useState("eur")
+  const [fromSel, setFromSel] = React.useState()
+  const [toSel, setToSel] = React.useState()
 
-  const [currKey, setCurrKey] = React.useState([])
-  const [currFull, setCurrFull] = React.useState([])
-
-  const [convData, setConvData] = React.useState("")
-  const [reverse, setReverse] = React.useState(false)
+  const [allCurr, setAllCurr] = React.useState([])
+  const [currNames, setCurrNames] = React.useState([])
 
 
-
-  React.useEffect(() => {
-    fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json")
-      .then(res => res.json())
-      .then(data => {
-        setCurrKey([...Object.keys(data)])
-        setCurrFull([...Object.values(data)])
-      })
-  }, [])
-
-
-  React.useEffect(() => {
-
-    if (!reverse) {
-      setToVal()
-      async function fetchDataNor() {
-        await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromSel}/${toSel}.json`)
-          .then(res => res.json())
-          .then(data => setConvData([...Object.values(data)][1]))
-        const convRes = parseFloat(fromVal) * convData
-        setToVal(convRes)
-      }
-
-      if (fromVal) {
-        fetchDataNor()
-      }
+  let toVal, fromVal
+  if (val != null) {
+    if (!inverse) {
+      fromVal = val
+      toVal = val * rate
     } else {
-      setFromVal()
-      async function fetchDataRev() {
-        await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${toSel}/${fromSel}.json`)
-          .then(res => res.json())
-          .then(data => setConvData([...Object.values(data)][1]))
-        const convRes = parseFloat(toVal) * convData
-        setFromVal(convRes)
-      }
-
-      if (toVal) {
-        fetchDataRev()
-      }
-
+      toVal = val
+      fromVal = toVal / rate
     }
-  }, [fromVal, fromSel, toVal, toSel])
-
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-    name === "fromVal" ? setFromVal(value) :
-      name === "toVal" ? setToVal(value) :
-        name === "fromSel" ? setFromSel(value) :
-          setToSel(value)
   }
 
-  function toggle(event) {
-    const { name } = event.target;
-    name === "fromVal" ? setReverse(false) : setReverse(true)
+  React.useEffect(() => {
+    fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json")
+      .then(res => res.json())
+      .then(data => {
+        const initial = Object.keys(Object.values(data)[1])[80]
+        setFromSel(Object.keys(data)[1])
+        setToSel(initial)
+        setAllCurr([...Object.keys(Object.values(data)[1])])
+        setRate(Object.values(data)[1][initial])
+      })
+    fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.min.json")
+      .then(res => res.json())
+      .then(data => setCurrNames([...Object.values(data)]))
+  }, [])
+
+  React.useEffect(() => {
+    if (fromSel != null && toSel != null) {
+      fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromSel}.min.json`)
+        .then(res => res.json())
+        .then(data => {
+          setRate(Object.values(data)[1][toSel])
+        })
+    }
+  }, [fromSel, toSel])
+
+  function handleFromChange(event) {
+    setVal(event.target.valueAsNumber)
+    setInverse(false)
+  }
+
+  function handleToChange(event) {
+    setVal(event.target.valueAsNumber)
+    setInverse(true)
   }
 
   return (
-    <div className="App">
-      <h1>React - Currency Converter</h1>
-      <Form
-        handleChange={handleChange}
-        fromVal={fromVal}
-        fromSel={fromSel}
-        toVal={toVal}
-        toSel={toSel}
-        currKey={currKey}
-        currFull={currFull}
-        toggle={toggle}
-      />
+    <div className="app" >
+      <div className="box">
+        <div className="fromForm">
+          <h1 className="title">React - Currency Exchange</h1>
+          <Form
+            handleChange={handleFromChange}
+            val={fromVal}
+            selVal={fromSel}
+            selChange={event => setFromSel(event.target.value)}
+            allCurr={allCurr}
+            currNames={currNames}
+          // currFull={currFull}
+          />
+        </div>
 
-      <p>Made completely from scratch with ❤️ by <a href="https://edmond-luu.github.io">Edmond Luu</a></p>
+        <div className="toForm">
+          <Form
+            handleChange={handleToChange}
+            val={toVal}
+            selVal={toSel}
+            selChange={event => setToSel(event.target.value)}
+            allCurr={allCurr}
+            currNames={currNames}
+          />
+        </div>
+      </div>
+      <div id="footer">
+        <p>Made completely from scratch with ❤️ by <a href="https://edmond-luu.github.io" target="_blank" rel="noreferrer">Edmond Luu</a></p>
+      </div>
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -4,32 +4,22 @@ export default function Form(props) {
 
 
     const optArray = []
-    
-    for (let i = 0; i < props.currKey.length; i++){
-        optArray.push(<option value={props.currKey[i]} key={props.currKey[i]}> {props.currFull[i]}  ({props.currKey[i].toUpperCase()})</option>)
-    }
 
+    for (let i = 0; i < props.allCurr.length; i++) {
+        optArray.push(<option value={props.allCurr[i]} key={props.allCurr[i]}> {props.allCurr[i].toUpperCase()} - {props.currNames[i]}</option>)
+    }
 
 
     return (
         <form>
-            <input type="number" name="fromVal" id="from" min={0} value={props.fromVal} onChange={props.handleChange} onClick={props.toggle}/>
+            <input className="input is-medium is-success" type="number" min={0} value={props.val} onChange={props.handleChange} />
 
-            <select name="fromSel" value={props.fromSel} onChange={props.handleChange}>
-                {optArray}
+            <div className="select is-info is-medium">
+                <select value={props.selVal} onChange={props.selChange}>
+                    {optArray}
 
-            </select>
-
-            <div className="spacer">
-                ➡️
+                </select>
             </div>
-
-            <input type="number" name="toVal" value={props.toVal} onChange={props.handleChange} onClick={props.toggle}/>
-            <select name="toSel" value={props.toSel} onChange={props.handleChange}>
-
-                {optArray}
-
-            </select>
 
         </form >
     )
